bfe455b82cb117fa3cd4d185bfb3106dade79ca9
Sawera Mushtaq <115520031+pythonworl@users.noreply.github.com>
2023-04-23 22:58:18 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.example.bmicalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("BMICalculator.fxml"));
+        primaryStage.setTitle("BMI Calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
